c457bfa11ba83dfafc21c16825c1141a4c177af7
yxb <yxb@idianyou.cn>
2019-11-05 11:38:41 +0800

    初始化

---
+++ src/main/java/cn/yxbin/design/DesignModelApplication.java
@@ -0,0 +1,13 @@
+package cn.yxbin.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModelApplication.class, args);
+    }
+
+}
